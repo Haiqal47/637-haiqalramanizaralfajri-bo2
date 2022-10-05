f47cc3f14054d5720393de1a2ec094269e8d8379
Haiqal Ramanizar Fajri <haiqalramanizaralfajri@gmail.com>
2022-10-05 22:26:06 +0700

    update materi sesi 08

--- sesi-08/practice/config/config.go
+++ sesi-08/practice/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 func DBInit() *gorm.DB {
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
+	dsn := "host=localhost user=postgres password=postgres dbname=gorm port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
